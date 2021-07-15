850f38da4c0a15f428dc0ceb02b3320935a2be0b
wjm <303787234@qq.com>
2021-07-15 09:01:07 +0800

    ++

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 
 
 const { Wechaty } = require('wechaty');
+const { PuppetPadplus } = require("wechaty-puppet-padplus");
 const config = require('./config/config')                           // 配置文件
 const { MessageClass } = require('./wechatyApiClass/messageClass')  // message API 类
 const {Init} = require("./wechatyApiClass/init");                   // 初始化 实体类 登录 等出 二维码
 
 
 let bot = new Wechaty({
-  name: config.name, // generate xxxx.memory-card.json and save login data for the next login
+  // name: config.name,
+  puppet: new PuppetPadplus({
+    token: 'puppet_wxwork_8c1b37387f44dd3f'
+  }),
+  name: "WeChat-Robot"
 });
 
 new Init(bot).init()
